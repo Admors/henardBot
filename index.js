8c0f1420ae08638ac5166366a7f941be789bb640
Admors <tthejuju@gmail.com>
2023-10-03 19:05:47 +0200

    03-10-23

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 ["commands", "cooldowns", "buttons", "selects"].forEach(x => { client[x] = new Collection(); });
-["CommandUtil", "EventUtil", "ButtonUtil", "SelectUtil"].forEach(handler => { require(`./utils/handlers/${handler}`)(client); });
 
 client.once(Events.ClientReady, c => {
     console.log(`-- Connexion au Service ${c.user.username} --\n--- Authentification Autorisée ---\n---- Service ${c.user.username} actif ----`);
