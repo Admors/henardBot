e34fb7755df0eb3796077ede6f53cc618e695c4f
Admors <tthejuju@gmail.com>
2023-10-03 18:43:43 +0200

    03-10-23

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+["commands", "cooldowns", "buttons", "selects"].forEach(x => { client[x] = new Collection(); });
+["CommandUtil", "EventUtil", "ButtonUtil", "SelectUtil"].forEach(handler => { require(`./utils/handlers/${handler}`)(client); });
+
 client.once(Events.ClientReady, c => {
     console.log(`-- Connexion au Service ${c.user.username} --\n--- Authentification Autorisée ---\n---- Service ${c.user.username} actif ----`);
 });
